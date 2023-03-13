8d8f140a3ee4e0968b4fe793bf0f1de2952ad386
abdullahit <abdullahit56@gmail.com>
2023-03-13 18:58:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 
 con = mysql.createConnection({
-    host: 'localhost',
-    user : 'root',
+    host: '13.233.95.37',
+    user : 'ubuntu',
     password : 'Csd@4850',
     database : 'nntmart'
 });
