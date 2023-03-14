e02a34ffa87e633a967a2c72ef23b2e63b01e483
abdullahit <abdullahit56@gmail.com>
2023-03-14 18:21:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ con = mysql.createConnection({
     host: '3.110.181.239',
     user : 'root',
     password : 'Csd@4850',
-    database : 'nntmart'
+    database : 'nntmart',
+    port:3306
 });
 module.exports = con;
